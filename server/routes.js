80617a093178d2c791a0a7ac8dd7a4e2d758e8c6
ewdegraaff <e.w.degraaff@gmail.com>
2015-07-28 19:45:03 +0200

    added clean offer endpoint

--- server/routes.js
+++ server/routes.js
@@ -9,6 +9,7 @@ var errors = require('./components/errors');
 module.exports = function(app) {
 
   // Insert routes below
+  app.use('/api/offers', require('./api/offer'));
   app.use('/api/customer', require('./api/customer'));
   app.use('/api/economy', require('./api/economy'));
   app.use('/api/things', require('./api/thing'));
